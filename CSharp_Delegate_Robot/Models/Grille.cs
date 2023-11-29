f31e51eaad10ea4a4f12716379235cf3399f9a93
LeeliAnna <gaelle.timmermans@gmail.com>
2023-11-29 20:22:02 +0100

    Ajoutez des fichiers projet.

---
+++ CSharp_Delegate_Robot/Models/Grille.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Delegate_Robot.Models
+{
+    internal class Grille
+    {
+        #region Propriétés
+
+        // Definition de la largeur de la grille toujours positive
+        public int Width 
+        {
+            get { return Width; }
+            private set 
+            {
+                if (value > 0)
+                {
+                    Width = 10;
+                }
+            }
+        }
+
+        // Definition de la hauteur de la grille toujours positive
+        public int Height 
+        {
+            get { return Height; }
+            private set
+            {
+                if (value > 0)
+                {
+                    Height = 10;
+                }
+            }
+        }
+
+        //Représente la position du point final à atteindre sur un axe horizontal(entre 0 et Width)
+        private int _finalX;
+        public int FinalX 
+        {
+            get { return _finalX; }
+            private set 
+            { 
+                if (value > 0 && value < Width) 
+                { 
+                    _finalX = value; 
+                }; 
+            } 
+        }
+
+        //Représente la position du point final à atteindre sur un axe veritcal(entre 0 et Height)
+        private int _finalY;
+        public int FinalY 
+        {
+            get
+            {
+                return _finalY;
+            }
+            private set 
+            {
+                if( value > 0 && value < Height)
+                {
+                    _finalY = value;
+                }
+            }
+        }
+        #endregion
+
+
+        #region Méthodes
+        //pour méthodes publiques :
+
+        //InitGame
+        //Aucun paramètre, ni retour : Permet d'initialiser le jeu en créant un Robot en position 0,0 sur la grille et de définir un point final aléatoire à atteindre. 
+
+        public void InitGame()
+        {
+
+        }
+
+        public void AfficherGrille()
+        {
+            for (int i = 0; i < Width; i++)
+            {
+                Console.WriteLine("----------------------------------");
+                for (int j = 0; j < Height; j++)
+                {
+                    Console.WriteLine("|  |");
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ CSharp_Delegate_Robot/Models/Robot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Delegate_Robot.Models
+{
+    internal class Robot
+    {
+        //Créer une class Robot qui a :
+
+        #region Propriétés
+        //pour propriétés en lecture seule:
+
+        //PositionX type entier : Représente la position du robot sur un axe horizontal(entre 0 et Width)
+        //PositionY type entier : Représente la position du robot sur un axe vertical(entre 0 et Height)
+        //Direction type Direction : Repésente l'axe de direction que le robot regarde (une énumération contenant Nord, Est, Sud, Ouest défini la direction)
+        //Grille type Grille : Représente la grille sur laquelle le robot se promène. 
+        #endregion
+
+        #region Méthodes
+        //pour méthodes publiques :
+
+        //Avancer
+        //Aucun paramètre nécessaire, et ne retourne rien : Permet de faire d'avancer le robot dans l'axe qu'il regarde.
+        //TournerGauche
+        //Aucun paramètre nécessaire, et ne retourne rien : Permet de changer la direction du robot vers la gauche.
+        //TournerDroite
+        //Aucun paramètre nécessaire, et ne retourne rien : Permet de changer la direction du robot vers la droite.
+        //EnregistrerOrdre
+        //Un paramètre de type OrdreRobot, et ne retourne rien : permet de sauvegarder une action du robot associer à un ordre.
+        //(OrdreRobot est une énumération contenant : Avancer, Gauche, Droite)
+        //Executer
+        //Aucun paramètre nécessaire, et ne retourne rien : Permet de lancer plusieurs ordre au robot qui ont été défini par l'utilisateur. 
+        #endregion
+    }
+}
